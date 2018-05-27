fe54108f980286558bd66e1303a5e5c60db43a52
hzsagkol <37846356+hzsagkol@users.noreply.github.com>
2018-05-27 03:14:07 +0300

    Update ayrıl.js

--- komutlar/ayrıl.js
+++ komutlar/ayrıl.js
@@ -1,35 +1,22 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const ayarlar = require('../ayarlar.json');
+
 exports.run = (client, message, args) => {
-  if (!message.guild) {
-  const ozelmesajuyari = new Discord.RichEmbed()
-  .setColor(0xFF0000)
-  .setTimestamp()
-  .setAuthor(message.author.username, message.author.avatarURL)
-  .addField(':warning: Uyarı :warning:', '`ayrıl` adlı komutu özel mesajlarda kullanamazsın.')
-  return message.author.sendEmbed(ozelmesajuyari); }
-  
-  message.guild.leave(message.guild.id);
-    const embed = new Discord.RichEmbed()
-  .setColor('RANDOM')
-  .setTitle('Sunucudan çıkardın;')
-  .setDescription(`**${message.guild.name}** sunucusundan beni çıkardın!`)
-  .setFooter('Rahatsız Adam', client.user.avatarURL)
-  .setTimestamp()
-  client.channels.get('443761073443831809').send(embed);
-
-
+  if (message.author.id !== ayarlar.sahip) return message.reply('Yapımcım Sen Değilsin');
+   message.channel.send('Bot Sunucudan Ayrıldı');
+   message.guild.leave()
 };
 
+
 exports.conf = {
-    enabled: true,
-    guildOnly: true,
-    aliases: ['ayrıl'],
-    permLevel: 4
-  };
-  
-  exports.help = {
-    name: 'ayrıl',
-    description: 'Komutun kullanıldığı sunucudan botu çıkarır.',
-    usage: 'an!ayrıl'
-  };
+  enabled: true,
+  guildOnly: false,
+  aliases: ['ayrıl'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'ayrıl',
+  description: 'Bot Sunucudan Ayrılır (YAPIMCI KOMUDU)',
+  usage: 'an!ayrıl'
+};
